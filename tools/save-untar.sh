d417f37456e850d6d6b11a3313d26aa917980266
sidorov <sidorov@majordomo.ru>
2019-07-24 21:48:44 +0300

    refactored

--- tools/save-untar.sh
+++ tools/save-untar.sh
@@ -1,6 +1,6 @@
 mkdir -p trash
 cd trash
-docker save docker-registry.intr/webservices/php71:master | tar xv --wildcards '*/layer.tar' | tee /tmp/tempstore  | xargs -n1 -I{} tar xvf {}
+docker save docker-registry.intr/webservices/apache2-php71:latest | tar xv --wildcards '*/layer.tar' | tee /tmp/tempstore  | xargs -n1 -I{} tar xvf {}
 cat /tmp/tempstore | xargs rm -rf
 find . -empty -type d -delete
 
