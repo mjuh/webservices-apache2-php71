7e3d221b80e8711d7c8af58944d46a78c9906ad3
Oleg Pykhalov <go.wigust@gmail.com>
2019-08-08 14:24:39 +0300

    Use php71Packages from overlay

--- default.nix
+++ default.nix
@@ -8,7 +8,7 @@ let
 
 inherit (builtins) concatMap getEnv toJSON;
 inherit (dockerTools) buildLayeredImage;
-inherit (lib) concatMapStringsSep firstNChars flattenSet dockerRunCmd buildPhpPackage mkRootfs;
+inherit (lib) concatMapStringsSep firstNChars flattenSet dockerRunCmd mkRootfs;
 inherit (lib.attrsets) collect isDerivation;
 inherit (stdenv) mkDerivation;
 
@@ -22,56 +22,6 @@ sh = dash.overrideAttrs (_: rec {
     ln -s dash "$out/bin/sh"
   '';
 });
-
-buildPhp71Package = args: buildPhpPackage ({ php = php71; } // args);
-
-php71Packages = {
-  redis = buildPhp71Package {
-    name = "redis";
-    version = "4.2.0";
-    sha256 = "7655d88addda89814ad2131e093662e1d88a8c010a34d83ece5b9ff45d16b380";
-  };
-
-  timezonedb = buildPhp71Package {
-    name = "timezonedb";
-    version ="2019.1";
-    sha256 = "0rrxfs5izdmimww1w9khzs9vcmgi1l90wni9ypqdyk773cxsn725";
-  };
-
-  rrd = buildPhp71Package {
-    name = "rrd";
-    version = "2.0.1";
-    sha256 = "39f5ae515de003d8dad6bfd77db60f5bd5b4a9f6caa41479b1b24b0d6592715d";
-    inputs = [ pkgconfig rrdtool ];
-  };
-
-  memcached = buildPhp71Package {
-    name = "memcached";
-    version = "3.1.3";
-    sha256 = "20786213ff92cd7ebdb0d0ac10dde1e9580a2f84296618b666654fd76ea307d4";
-    inputs = [ pkgconfig zlib.dev libmemcached ];
-    configureFlags = [
-      "--with-zlib-dir=${zlib.dev}"
-      "--with-libmemcached-dir=${libmemcached}"
-    ];
-  };
-
-  imagick = buildPhp71Package {
-    name = "imagick";
-    version = "3.4.3";
-    sha256 = "1f3c5b5eeaa02800ad22f506cd100e8889a66b2ec937e192eaaa30d74562567c";
-    inputs = [ pkgconfig imagemagick.dev pcre ];
-    configureFlags = [ "--with-imagick=${imagemagick.dev}" ];
-  };
-
-  libsodiumPhp = buildPhp71Package {
-    name = "libsodium";
-    version = "2.0.21";
-    sha256 = "1sqz5987mg02hd90v695606qj5klpcrvzwfbj0yvg60vakbk3sz4";
-    inputs = [ libsodium.dev ];
-    configureFlags = [ "--with-sodium=${libsodium.dev}" ];
-  };
-};
 
 rootfs = mkRootfs {
   name = "apache2-php71-rootfs";
