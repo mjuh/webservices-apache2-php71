ac33df5fec40727d3a8f7e025ebc7ce69f7253f4
Oleg Pykhalov <go.wigust@gmail.com>
2019-09-19 15:39:48 +0300

    Build image in this repository

--- default.nix
+++ default.nix
@@ -1,7 +1,62 @@
+{}:
+
 with import <nixpkgs> {
   overlays = [
     (import (builtins.fetchGit { url = "git@gitlab.intr:_ci/nixpkgs.git"; ref = "master"; }))
   ];
 };
 
-php71-image
+let
+  inherit (builtins) concatMap getEnv toJSON;
+  inherit (dockerTools) buildLayeredImage;
+  inherit (lib) concatMapStringsSep firstNChars flattenSet dockerRunCmd mkRootfs;
+  inherit (lib.attrsets) collect isDerivation;
+  inherit (stdenv) mkDerivation;
+
+  php71DockerArgHints = lib.phpDockerArgHints php.php71;
+
+  rootfs = mkRootfs {
+    name = "apache2-rootfs";
+    src = ./rootfs;
+    inherit curl coreutils findutils apacheHttpdmpmITK apacheHttpd
+      mjHttpErrorPages postfix s6 execline;
+    php71 = php.php71;
+    mjperl5Packages = mjperl5lib;
+    ioncube = ioncube.v71;
+    s6PortableUtils = s6-portable-utils;
+    s6LinuxUtils = s6-linux-utils;
+    mimeTypes = mime-types;
+    libstdcxx = gcc-unwrapped.lib;
+  };
+
+in
+
+pkgs.dockerTools.buildLayeredImage rec {
+  maxLayers = 124;
+  name = "docker-registry.intr/webservices/apache2-php71";
+  tag = "latest";
+  contents = [
+    rootfs
+    tzdata
+    locale
+    postfix
+    sh
+    coreutils
+    perl
+  ] ++ collect isDerivation phpPackages.php71Packages;
+  config = {
+    Entrypoint = [ "${rootfs}/init" ];
+    Env = [
+      "TZ=Europe/Moscow"
+      "TZDIR=${tzdata}/share/zoneinfo"
+      "LOCALE_ARCHIVE_2_27=${locale}/lib/locale/locale-archive"
+      "LOCALE_ARCHIVE=${locale}/lib/locale/locale-archive"
+      "LC_ALL=en_US.UTF-8"
+    ];
+    Labels = flattenSet rec {
+      ru.majordomo.docker.arg-hints-json = builtins.toJSON php71DockerArgHints;
+      ru.majordomo.docker.cmd = dockerRunCmd php71DockerArgHints "${name}:${tag}";
+      ru.majordomo.docker.exec.reload-cmd = "${apacheHttpd}/bin/httpd -d ${rootfs}/etc/httpd -k graceful";
+    };
+  };
+}
